8c0a8b08adc0df805ae925205fab79db03e393fe
Angel <ang_vzrz@live.com.mx>
2022-12-20 23:55:18 -0600

    Set, update and display timer

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -7,6 +7,7 @@ import SwiperCore, {
   Zoom,
 } from 'swiper';
 import { IonicSlides } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
 
 SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 
@@ -18,40 +19,91 @@ SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 })
 export class HomePage {
   speed: string | undefined;
-  isModalOpen: boolean = false;
-  shutdownTime: string = '';
+  isModalOpen: boolean;
+  initialDateTime: string | undefined;
+  shutdownTime: string | undefined;
+  time: BehaviorSubject<string> = new BehaviorSubject('Start timer');
+  timer: number = 0; // in seconds
 
-  constructor() {}
+  constructor() {
+    this.isModalOpen = false;
+  }
 
   setFan(speed: string) {
     this.speed = speed;
   }
 
+  startTimer(dateTime: any) {
+    const { hours, minutes } = this.getDateClock(dateTime);
+    this.setTimer(hours, minutes);
+
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+
+    this.showModal(false);
+  }
+
+  updateTime() {
+    let hours: number = this.timer / 60 / 60;
+    let minutes: number = (this.timer / 60) % 60;
+    let seconds: number = this.timer % 60;
+
+    const hoursFormat: string = String('0' + Math.floor(hours)).slice(-2);
+    const minutesFormat: string = String('0' + Math.floor(minutes)).slice(-2);
+    const secondsFormat: string = String('0' + Math.floor(seconds)).slice(-2);
+
+    const timerFormat = hoursFormat + ':' + minutesFormat + ':' + secondsFormat;
+    this.time.next(timerFormat);
+
+    --this.timer;
+  }
+
   showModal(isOpen: boolean) {
+    const pickerDefaultDate = new Date();
+    pickerDefaultDate.setHours(1, 29);
+    this.timeChanged(pickerDefaultDate.toISOString());
     this.isModalOpen = isOpen;
   }
 
-  timeChanged(dateTime: any) {
-    const pickerDate = new Date(dateTime);
-    const pickerTime =
-      pickerDate.getHours() * 60 * 60 * 1000 +
-      pickerDate.getMinutes() * 60 * 1000;
-    const currentTime: number = new Date().getTime();
-    const shutdownDate = new Date(pickerTime + currentTime);
-    let midday: string = '';
-    let shutdownHour = shutdownDate.getHours();
+  setTimer(hours: number, minutes: number) {
+    const hoursInSeconds = hours * 60 * 60;
+    const minutesInSeconds = minutes * 60;
 
-    if (shutdownDate.getHours() > 11) {
+    this.timer = hoursInSeconds + minutesInSeconds;
+  }
+
+  setShutdownTime(hour: number, minute: number) {
+    let midday: string = '';
+
+    if (hour > 11) {
       midday = 'pm';
-      shutdownHour -= 12;
+      hour -= 12;
     } else {
       midday = 'am';
     }
 
-    this.shutdownTime = `${shutdownHour}:${shutdownDate.getMinutes()} ${midday}`;
+    this.shutdownTime = `${hour}:${minute} ${midday}`;
   }
 
-  setTimer() {
-    this.showModal(false);
+  getDateClock(dateTime: any): {
+    hours: number;
+    minutes: number;
+  } {
+    const date: Date = new Date(dateTime);
+    const hours: number = date.getHours();
+    const minutes: number = date.getMinutes();
+
+    return { hours: hours, minutes: minutes };
+  }
+
+  timeChanged(dateTime: any) {
+    const { hours, minutes } = this.getDateClock(dateTime);
+    const pickerTime = hours * 60 * 60 * 1000 + minutes * 60 * 1000;
+
+    const currentTime: number = new Date().getTime();
+    const shutdownDate = new Date(pickerTime + currentTime);
+
+    this.setShutdownTime(shutdownDate.getHours(), shutdownDate.getMinutes());
   }
 }
